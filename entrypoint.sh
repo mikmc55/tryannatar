51339169654ee4e964040ea12def37ebd68edc98
mikmc55 <70898105+mikmc55@users.noreply.github.com>
2024-07-03 18:25:50 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
+# Set up Prometheus multiproc directory
 export PROMETHEUS_MULTIPROC_DIR="${PROMETHEUS_MULTIPROC_DIR:-/tmp/prometheus/metrics}"
+echo "Using Prometheus multiproc directory: $PROMETHEUS_MULTIPROC_DIR"
+
+# Remove and recreate directory
 if [ -d "$PROMETHEUS_MULTIPROC_DIR" ]; then
+    echo "Removing existing Prometheus multiproc directory..."
     rm -rf "$PROMETHEUS_MULTIPROC_DIR"
 fi
+echo "Creating Prometheus multiproc directory..."
 mkdir -p "$PROMETHEUS_MULTIPROC_DIR"
 
-python run.py
+# Start the application
+echo "Starting the application..."
+exec python run.py
